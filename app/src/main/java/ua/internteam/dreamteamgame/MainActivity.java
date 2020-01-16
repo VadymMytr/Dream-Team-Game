3900d527da018ce4697418b37bb36e0a1a413ddb
VadymMytr <v.my@ukr.net>
2020-01-16 11:53:37 +0200

    Add MediaPlayer class to create simple exo player. Add MediaPlayerSource class to create media source by URI.

--- app/src/main/java/ua/internteam/dreamteamgame/MainActivity.java
+++ app/src/main/java/ua/internteam/dreamteamgame/MainActivity.java
@@ -2,7 +2,21 @@ package ua.internteam.dreamteamgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ext.rtmp.RtmpDataSourceFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 
 public class MainActivity extends AppCompatActivity {
 
