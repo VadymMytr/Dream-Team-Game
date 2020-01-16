637a8d82c957258b977d747ae163e8438c08ed74
treshkaIntern <intern.mir@gmail.com>
2020-01-16 14:00:55 +0200

    added main functionality

--- app/src/main/java/ua/internteam/dreamteamgame/MainActivity.java
+++ app/src/main/java/ua/internteam/dreamteamgame/MainActivity.java
@@ -2,6 +2,7 @@ package ua.internteam.dreamteamgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.setIP);
+        EditText editText = (EditText) findViewById(R.id.urlET);
+
+
     }
+
+    void onClickM(String url){
+        Intent intent = new Intent(this, MediaPlayerActivity.class);
+        intent.putExtra("streamURL",url);
+        startActivity(intent);
+
+    }
+
 }
---
+++ app/src/main/java/ua/internteam/dreamteamgame/MediaPlayerActivity.java
@@ -0,0 +1,45 @@
+package ua.internteam.dreamteamgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ext.rtmp.RtmpDataSourceFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+
+import java.util.Objects;
+
+public class MediaPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_player);
+
+        View overlay = findViewById(R.id.activity_media_player);
+
+        overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        Bundle bundle = getIntent().getExtras();
+        String url = "";
+        if (bundle!=null){
+            url = bundle.getString("streamURL");
+            bundle.remove("streamURL");
+        }
+
+    }
+}
