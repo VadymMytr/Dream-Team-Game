0bf97f37308f6641227db59e62b0ca441e509eaf
Daria1305 <43953506+Daria1305@users.noreply.github.com>
2020-01-22 11:45:53 +0200

    a lot of uncommited changes.

--- app/src/main/java/ua/internteam/dreamteamgame/MainActivity.java
+++ app/src/main/java/ua/internteam/dreamteamgame/MainActivity.java
@@ -1,8 +1,21 @@
 package ua.internteam.dreamteamgame;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +23,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.setIP);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickM();
+            }
+        });
+
+    }
+
+    void onClickM(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://10.0.1.125:8080/answer/add") // Адрес сервера
+                .addConverterFactory(GsonConverterFactory.create()) // говорим ретрофиту что для сериализации необходимо использовать GSON
+                .build();
+
+        Server service = retrofit.create(Server.class);
+        Call<List<AddResult>> call = service.addAnswer("TEST");
+        call.enqueue(new Callback<List<AddResult>>() {
+            @Override
+            public void onResponse(Call<List<AddResult>> call, Response<List<AddResult>> response) {
+                if (response.isSuccessful()) {
+                    // запрос выполнился успешно, сервер вернул Status 200
+                } else {
+                    // сервер вернул ошибку
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<AddResult>> call, Throwable t) {
+                // ошибка во время выполнения запроса
+            }
+        });
     }
 }
+
