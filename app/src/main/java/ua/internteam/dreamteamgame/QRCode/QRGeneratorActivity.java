38edddd899f245f9b54389854221bb37acde5118
VadymMytr <v.my@ukr.net>
2020-01-31 15:47:46 +0200

    change ui

--- app/src/main/java/ua/internteam/dreamteamgame/QRCode/QRGeneratorActivity.java
+++ app/src/main/java/ua/internteam/dreamteamgame/QRCode/QRGeneratorActivity.java
@@ -1,7 +1,9 @@
 package ua.internteam.dreamteamgame.QRCode;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +15,7 @@ import com.google.zxing.WriterException;
 
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
+import ua.internteam.dreamteamgame.ActivityStyleHandler;
 import ua.internteam.dreamteamgame.MediaPlayerActivity;
 import ua.internteam.dreamteamgame.R;
 import ua.internteam.dreamteamgame.api.entity.Team;
@@ -85,5 +88,24 @@ public class QRGeneratorActivity extends AppCompatActivity {
         }
 
         startActivity(intent);
+    }
+
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    private void setStyle() {
+        ActivityStyleHandler handler = new ActivityStyleHandler(this,
+                findViewById(R.id.activity_media_player));
+        handler.setStyle();
     }
 }
